57d036340348ec29aae738cbe056fe4a046e38a8
MakiAziz <38784238+MakiAziz@users.noreply.github.com>
2018-05-08 19:59:22 +0200

    Ispocetka
    Izbacili smo sve i krecemo ispocetka sa text avanturom

---
+++ Underslavia/Assets/Scripts/PlayGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayGame : MonoBehaviour {
+
+
+
+	public void Play_Game (){
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
